58c15f4d6ed02c33b0d9d70569aba5835bdc0276
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2017-12-29 15:59:45 -0800

    initial build

---
+++ main.js
@@ -0,0 +1,60 @@
+const SHA256 = require('crypto-js/sha256')
+
+class Block {
+  constructor(index, timestamp, data, previousHash = ''){
+      this.index =index;
+      this.timestamp = timestamp;
+      this.data = data;
+      this.previousHash = previousHash;
+      this.hash = this.calculateHash();
+  }
+  calculateHash(){
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+}
+
+class Blockchain {
+  constructor(){
+    this.chain = [this.createGenesisBlock()];
+  }
+  createGenesisBlock(){
+    return new Block(0, "01/01/2018", "Genesis Block", "0");
+  }
+
+  getLatestBlock(){
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock){
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid(){
+    for(let i=1; i < this.chain.length; i++){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i-1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash){
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+let kahlilCoin = new Blockchain();
+kahlilCoin.addBlock(new Block(1, "11/03/2018", { amount: 4 }));
+kahlilCoin.addBlock(new Block(2, "11/05/2018", { amount: 10 }));
+
+console.log('Is blockchain valid? ' + kahlilCoin.isChainValid());
+
+kahlilCoin.chain[1].data = { amount: 100 };
+console.log('Is blockchain valid? ' + kahlilCoin.isChainValid());
+
+// console.log(JSON.stringify(kahlilCoin, null, 4));
